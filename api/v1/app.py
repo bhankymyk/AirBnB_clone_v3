1b4d86ff9ae6c0577b99edf4d7c651c0a26379cf
bhankymyk <bankole123michael@gmail.com>
2022-10-22 14:24:52 +0100

    app.py added

---
+++ api/v1/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+API application file
+"""
+
+
+from os import getenv
+from models import storage
+from flask import Flask, jsonify
+from flask import Blueprint, make_response
+from api.vi.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """Close storage session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """returns a JSON-formatted 404 status code response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == '__main__':
+    app.run(
+        host=getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=getenv('HBNB_API_PORT', 5000),
+        threaded=True
+    )
