8361b539d06f4117a8f3e70d91b094fecc1db355
bhankymyk <bankole123michael@gmail.com>
2022-10-24 13:51:27 +0100

    Classes added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,20 +7,25 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
-def api_status():
-    """returns a JSON: "status": "OK" """
-    return jsonify(status='OK')
+classes = {"amenities": "Amenity",
+           "cities": "City",
+           "places": "Place",
+           "reviews": "Review",
+           "states": "State",
+           "users": "User"}
 
 
-@app_views.route('/status')
-def api_stat():
-    """retrieves the number of each objects by type"""
-    return jsonify({
-                    "amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")
-                  })
+@app_views.route('/status', strict_slashes=False)
+def test():
+    """check status of the api"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def counts():
+    """endpoint that retrieves the number of each objects by typ"""
+    from models import storage
+    count_dict = {}
+    for k, v in classes.items():
+        count_dict[k] = storage.count(v)
+    return jsonify(count_dict)
